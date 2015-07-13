aa2807e9004714e54f82a4a9f6ee861b64265047
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-13 11:20:35 -0500

    while challenge

--- while.php
+++ while.php
@@ -1,9 +1,17 @@
 <?php
 
     $test = 5;
-    
+    $conditionOne = 4;
+    $conditionTwo = 2;
+
     while ($test <= 15) {
-        echo "$test" . PHP_EOL;
+        if ($test % $conditionOne == 0 && $test % $conditionTwo == 0) {
+            echo "$test is divisible by both {$conditionOne} and {$conditionTwo}" . PHP_EOL;
+        } else if ($test % $conditionTwo == 0) {
+            echo "$test is divisible by {$conditionTwo}" . PHP_EOL;
+        } else {
+            echo "$test" . PHP_EOL;      
+        }
         $test++;
     }
 
