f63bd6a70eefc7aef73e151cd9af037854a05f63
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-15 11:27:57 -0500

    replaced //add code here

--- arithmetic.php
+++ arithmetic.php
@@ -2,22 +2,22 @@
 
 function add($a, $b)
 {
-    echo $a + $b;
+    return $a + $b;
 }
 
 function subtract($a, $b)
 {
-    // Add code here
+    return $a - $b;
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+    return $a * $b;
 }
 
 function divide($a, $b)
 {
-    // Add code here
+    return $a / $b;
 }
 
 // Add code to test your functions here
