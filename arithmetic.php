1f312c30599ff8306ea9bba6f13040be6bdfe9aa
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-15 11:31:57 -0500

    called each function with real numbers

--- arithmetic.php
+++ arithmetic.php
@@ -20,6 +20,22 @@ function divide($a, $b)
     return $a / $b;
 }
 
-// Add code to test your functions here
+echo add(5, 10) . PHP_EOL;
 
+echo subtract(20, 4) . PHP_EOL;
+
+echo multiply(4, 3) . PHP_EOL;
+
+echo divide(100, 50) . PHP_EOL;
+
+
+
+// Fill in the // Add code here blocks to make each function echo the proper result.
+// Add code after functions that calls each function with real numbers.
+// Verify the output of each test.
+// Add a function modulus that finds the modulus of 2 numbers.
+// Add test code and verify the output of modulus.
+// Create variables $a and $b at the top of your script and give them different values.
+	// Watch what happens (or, does not happen) to your function output as you set and modify $a & $b outside of your functions.
+	// Think carefully about how this behavior differs from the way JavaScript handles variables.
 ?>
