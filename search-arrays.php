6f369e20ee67186b6fe5c9263625076f316a017f
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-16 12:04:11 -0500

    created function to return true/false if value is found

---
+++ search-arrays.php
@@ -0,0 +1,34 @@
+<?php
+
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+//Create a function that returns TRUE or FALSE if an array value is found.
+    //Search for Tina and Bob in $names.
+    //Make sure it works as expected.
+function isInArray ($person, $names){
+    $result = array_search($person, $names);
+
+    if ($result !== false){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if (isInArray('Tina', $names)){
+    echo "Tina is in the array." . PHP_EOL;
+} else {
+    echo "Tina is not in the array." . PHP_EOL;
+}
+
+if (isInArray('Bob', $names)){
+    echo "Bob is in the array." . PHP_EOL;
+} else {
+    echo "Bob is not in the array" . PHP_EOL;
+}
+
+//Create a function to compare 2 arrays that returns the number of values in common between the arrays.
+    //Use the 2 example arrays and make sure your solution uses array_search().
+?>
