2a9f778581286dda662c513071d09313be933b46
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-16 10:46:17 -0500

    sort people alphabetically

--- technology-companies.php
+++ technology-companies.php
@@ -49,12 +49,17 @@ $companies = [
 // print_r($companies);
 
 //Sort the $companies array by company name and output the results.
-ksort($companies);
-print_r($companies);
+// ksort($companies);
+// print_r($companies);
 
 //Sort the people in each company alphabetically.
     //You will need to use a foreach loop and will need to reassign each inner array after sorting.
     //Output the result.
+foreach ($companies as $key => $people){
+    echo $key;
+    sort($people);
+    print_r($people) . PHP_EOL;
+}
 
 //Sort the companies from "biggest" to "smallest".
     //This may be easier than you think, but be sure you don't loose the company names!
