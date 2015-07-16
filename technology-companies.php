82a5fac4602bb2e074a83b57b388afd0849a08ee
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-16 13:49:57 -0500

    edited content

--- technology-companies.php
+++ technology-companies.php
@@ -63,7 +63,7 @@ $companies = [
 
 //Sort the companies from "biggest" to "smallest".
     //This may be easier than you think, but be sure you don't loose the company names!
-arsort($companies);
+sort($companies);
 print_r($companies);
 
 ?>
