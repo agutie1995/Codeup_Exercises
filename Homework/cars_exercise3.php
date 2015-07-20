db27d833b28e273180096362c33ea2f94d45fb67
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-20 11:29:07 -0500

    homework exercises

---
+++ Homework/cars_exercise3.php
@@ -0,0 +1,40 @@
+<?php
+
+$cars = array(
+    'Toyota Solara' => array(
+        'year' => 2001,
+        'doors' => 2,
+        'color' => 'gray',
+        'mileage' => 100000,
+        'sunroof' => true,
+        'convertable' => false,
+        'license' => 'JSRULZ',
+    ),
+
+    'Honda Odyssey' => array(
+        'year' => 2004, 
+        'doors' => 4,
+        'color' => 'maroon',
+        'mileage' => 60000,
+        'sunroof' => false,
+        'convertable' => false,
+        'license' => 'ILUVPHP',
+    ),
+
+    'Cadillac Escalade' => array(
+        'year' => 2014,
+        'doors' => 4,
+        'color' => 'white',
+        'mileage' => 5000,
+        'sunroof' => false,
+        'convertable' => false,
+        'license' => 'HTML5',
+    ),
+
+);
+
+foreach ($cars as $make => $detail) {
+    
+}
+
+?>
